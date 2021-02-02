48456f75b8c6a00c03e4d64af76bae1392148633
Gabriel Staples <ercaguy@gmail.com>
2021-02-01 23:12:34 -0800

    Update .clang-format and run formatter
    Put C++ classs initializer list items all on their own line.
    
    	modified:   .clang-format
    	modified:   src/eRCaGuy_X9C_digital_pot.cpp
    	modified:   src/eRCaGuy_X9C_digital_pot.h

--- src/eRCaGuy_X9C_digital_pot.cpp
+++ src/eRCaGuy_X9C_digital_pot.cpp
@@ -1,18 +1,17 @@
 
 #include "eRCaGuy_X9C_digital_pot.h"
 
-
 #define SELECT_CHIP() digitalWrite(_CS_PIN, LOW)
 #define DESELECT_CHIP() digitalWrite(_CS_PIN, HIGH)
 
 #define TOGGLE_INC_PIN_HIGH_TO_LOW()  \
-    do { \
+    do                                \
+    {                                 \
         digitalWrite(_INC_PIN, HIGH); \
-        digitalWrite(_INC_PIN, LOW); \
+        digitalWrite(_INC_PIN, LOW);  \
     } while (false)
 
-
-X9C103::X9C103(uint8_t cs_pin, uint8_t inc_pin, uint8_t up_down_pin):
+X9C103::X9C103(uint8_t cs_pin, uint8_t inc_pin, uint8_t up_down_pin) :
     _CS_PIN(cs_pin),
     _INC_PIN(inc_pin),
     _UP_DOWN_PIN(up_down_pin)
@@ -65,7 +64,7 @@ void X9C103::setWiperPosition(uint8_t position)
         indexPosition();
     }
 
-    int16_t delta = position - _wiper_pos; // desired - actual
+    int16_t delta = position - _wiper_pos;  // desired - actual
 
     if (delta > 0)
     {
--- src/eRCaGuy_X9C_digital_pot.h
+++ src/eRCaGuy_X9C_digital_pot.h
@@ -4,7 +4,7 @@
 
 class X9C103
 {
-public:
+  public:
     /// Constructor
     X9C103(uint8_t cs_pin, uint8_t inc_pin, uint8_t up_down_pin);
 
@@ -28,15 +28,17 @@ public:
     /// Minimum absolute wiper position allowed
     static constexpr int16_t WIPER_MINIMUM = 1;
     /// Maximum absolute wiper position allowed
-    static constexpr int16_t WIPER_MAXIMUM = 100; /////////// add code to constrain and prevent over/underflows!
+    static constexpr int16_t WIPER_MAXIMUM =
+        100;  /////////// add code to constrain and prevent over/underflows!
 
-private:
+  private:
     /// timestamp of the last time the wiper position was stored into non-volatile memory
     uint32_t _time_last_stored_ms = 0 - NON_VOLATILE_MEMORY_STORE_TIME_MS;
 
-    const uint8_t _CS_PIN; // Chip Select pin
-    const uint8_t _INC_PIN; // Incremement/Decrement pin
-    const uint8_t _UP_DOWN_PIN; // Up/Down selection pin (affects which direction the Inc pin works)
+    const uint8_t _CS_PIN;   // Chip Select pin
+    const uint8_t _INC_PIN;  // Incremement/Decrement pin
+    const uint8_t
+        _UP_DOWN_PIN;  // Up/Down selection pin (affects which direction the Inc pin works)
 
     /// Current absolute wiper position (1 to 100)
     uint8_t _wiper_pos = 0;
