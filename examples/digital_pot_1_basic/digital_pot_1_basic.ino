ffe71c5ee87519d6c85a90c938218f9e1c9345e7
Gabriel Staples <ercaguy@gmail.com>
2021-02-03 08:41:18 -0800

    Update comments in examples to specify you can change the pins

--- examples/digital_pot_1_basic/digital_pot_1_basic.ino
+++ examples/digital_pot_1_basic/digital_pot_1_basic.ino
@@ -20,6 +20,12 @@ Vw          -             Wiper output---this is your output voltage signal to c
 
 #include <eRCaGuy_X9C_digital_pot.h>
 
+// This code works on any and all Arduino or Arduino-compatible boards. Set these pins to any
+// `digitalWrite()`-capable pin number as you see fit. Using pins 0 and 1, however, is generally not
+// recommended, as they are for serial communication to upload your program. Therefore, for the
+// Uno/Nano/Pro Mini type ATmega328-based boards, I recommend Digital pins 2 through 13, inclusive,
+// or Analog pins A0 through A5, inclusive. NB: If your ATmega168/328-based board has an A6 or A7
+// pin, these pins do NOT support `digitalWrite()`/`digitalRead()`!
 constexpr uint8_t CS_PIN = 2;
 constexpr uint8_t INC_PIN = 3;
 constexpr uint8_t UP_DOWN_PIN = 4;
